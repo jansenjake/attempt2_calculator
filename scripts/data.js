135f27ace314d2f3fb20e82fbb99f06c4116110c
Jake_Jansen <jansenjake599@gmail.com>
2022-12-07 15:07:42 +0200

    first commit

---
+++ scripts/data.js
@@ -0,0 +1,8 @@
+let input = document.getElementsByClassName('in'),
+    number = document.getElementsByClassName('numbers'),
+    operartor = document.getElementsByClassName('operators'),
+    result = document.getElementsByClassName('equal'),
+    clear = document.getElementsByClassName('btn-clear'),
+    resultDisplayed = false;
+
+
